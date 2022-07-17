e88aa53dc1bb289c04a4a727334ad0037dd1aa02
Oskar Grabowski <graab.oskar@gmail.com>
2022-07-17 11:55:44 +0200

    day 8 start

---
+++ day_8/rust/target/rls/debug/deps/rust-6c21634c9088b6c9.d
@@ -0,0 +1,5 @@
+c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-6c21634c9088b6c9.rmeta: src\main.rs
+
+c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-6c21634c9088b6c9.d: src\main.rs
+
+src\main.rs:
---
+++ day_8/rust/target/rls/debug/deps/rust-7686add47371c6bb.d
@@ -0,0 +1,5 @@
+c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-7686add47371c6bb.rmeta: src\main.rs
+
+c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-7686add47371c6bb.d: src\main.rs
+
+src\main.rs:
