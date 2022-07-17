963355261ffd0eee323240833e62f81841a5af79
Oskar Grabowski <graab.oskar@gmail.com>
2022-07-17 12:02:32 +0200

    Updated gitignore to exclude target in nested folders

--- day_8/rust/target/rls/debug/deps/rust-6c21634c9088b6c9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-6c21634c9088b6c9.rmeta: src\main.rs
-
-c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-6c21634c9088b6c9.d: src\main.rs
-
-src\main.rs:
--- day_8/rust/target/rls/debug/deps/rust-7686add47371c6bb.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-7686add47371c6bb.rmeta: src\main.rs
-
-c:\Users\Oskar\source\repos\Advent-of-code-2019\day_8\rust\target\rls\debug\deps\rust-7686add47371c6bb.d: src\main.rs
-
-src\main.rs:
